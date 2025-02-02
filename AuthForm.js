9c7c84e32cee1c3ca9a7f2b4678f8f1a476719dd
Anishush <80246103+anishshshsh@users.noreply.github.com>
2025-02-02 23:18:23 +0530

    Updated-1 AuthForm.js

--- AuthForm.js
+++ AuthForm.js
@@ -1,36 +1,35 @@
+import React, { useState } from 'react';
+import styles from './AuthForm.module.css'; // Importing CSS as a module
 
-import React, {useState} from 'react';
-
-export default function AuthForm() 
-{
+export default function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
+
   return (
-    <div className='container'>
-      <div className='form-container'>
-        <div className='form-toggle'>
-          <button className={isLogin ? 'active' : ""} onClick={() => setIsLogin(true)}>Login</button>
-          <button className={!isLogin ? 'active' : ""}onClick={() => setIsLogin(false)}>SignUp</button>
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <div className={styles.formToggle}>
+          <button className={isLogin ? styles.active : ""} onClick={() => setIsLogin(true)}>Login</button>
+          <button className={!isLogin ? styles.active : ""} onClick={() => setIsLogin(false)}>SignUp</button>
         </div>
-        { isLogin ? <>
-        <div className='form'>
-          <h2>Login Form</h2>
-          <input type='email' placeholder='Email'/>
-          <input type='password' placeholder='password'/>
-          <a href='#'>Forgot Password?</a>
-          <button>Login</button>
-          <p>Not a Member? <a href='#' onClick={()=> setIsLogin(false)}>SignUp Now</a></p>
-        </div>
-        </> : <>
-        <div className='form'>
-        <h2>Signup Form</h2>
-        <input type='email' placeholder='Email' />
-        <input type='password' placeholder='password'/>
-        <input type='password' placeholder='Confirm password' />
-        <button>SignUp</button>
-        </div>
-        </>}
+        { isLogin ? (
+          <div className={styles.form}>
+            <h2>Login Form</h2>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forgot Password?</a>
+            <button>Login</button>
+            <p>Not a Member? <a href="#" onClick={() => setIsLogin(false)}>SignUp Now</a></p>
+          </div>
+        ) : (
+          <div className={styles.form}>
+            <h2>Signup Form</h2>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <button>SignUp</button>
+          </div>
+        )}
       </div>
     </div>
-
-)
+  );
 }
