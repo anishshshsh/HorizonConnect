64d06e429b8da691abca27c99f0933afa66e20da
Anishush <80246103+anishshshsh@users.noreply.github.com>
2025-02-13 21:41:30 +0530

    Updated-1 index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles.css"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  <BrowserRouter> {/* ✅ This should be only in index.js */}
     <App />
-  </Router>
+  </BrowserRouter>
 );
